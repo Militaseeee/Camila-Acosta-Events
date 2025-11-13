89e9eda347a3b582ef001126b7e41166c5b5eaaf
militaseeee <camilitaacosta2001@gmail.com>
2025-11-13 12:08:32 -0500

    feature: create mapper, dto with venues an events

---
+++ events-venues/src/main/java/com/events_cav/events_venues/dto/EventRequest.java
@@ -0,0 +1,14 @@
+package com.events_cav.events_venues.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record EventRequest(
+        @NotBlank(message = "The event name is required")
+        String name,
+
+        @NotBlank(message = "The date is mandatory")
+
+        @NotNull(message = "Venue ID is required")
+        Long id_venue
+) {}
