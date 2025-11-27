63a0dfb6b13fac77f23ea8c324f9f794b80ef37a
Militaseeee <camilitaacosta2001@gmail.com>
2025-11-26 21:52:41 -0500

    feat: start with US5

---
+++ events-venues/src/main/java/com/events_cav/events_venues/infrastructure/adapters/input/web/validation/groups/ValidationGroups.java
@@ -0,0 +1,11 @@
+package com.events_cav.events_venues.infrastructure.adapters.input.web.validation.groups;
+
+public interface ValidationGroups {
+
+    // Interfaz para la validación durante la creación (POST)
+    public interface OnCreate {}
+
+    // Interfaz para la validación durante la actualización (PUT)
+    public interface OnUpdate {}
+
+}
