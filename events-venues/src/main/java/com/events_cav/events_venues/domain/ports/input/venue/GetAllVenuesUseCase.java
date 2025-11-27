c38b79243d9dcce351d784849e9d12c6edec97e3
Militaseeee <camilitaacosta2001@gmail.com>
2025-11-26 21:17:35 -0500

    fix: update interface getAll in venue -> impl pageable

--- events-venues/src/main/java/com/events_cav/events_venues/domain/ports/input/venue/GetAllVenuesUseCase.java
+++ events-venues/src/main/java/com/events_cav/events_venues/domain/ports/input/venue/GetAllVenuesUseCase.java
@@ -1,9 +1,13 @@
 package com.events_cav.events_venues.domain.ports.input.venue;
 
 import com.events_cav.events_venues.domain.model.VenueModel;
+import org.springframework.data.domain.Page;
+
+import java.awt.print.Pageable;
 import java.util.List;
 
 // Define el caso de uso para listar todos los Venues.
 public interface GetAllVenuesUseCase {
-    List<VenueModel> getAll();
+//    List<VenueModel> getAll();
+    Page<VenueModel> getAll(Pageable pageable);
 }
